c2142795c0e7fa64ce6579e5226efdfa3b4127e2
Awoh Negheie <awohnegheie@gmail.com>
2019-05-08 23:57:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,10 @@ if phrase == "I LOVE YOU GRANDMA!"
 # and yells back
 elsif phrase == phrase.downcase
   		return "HUH?! SPEAK UP, SONNY!"
-
+    elsif phrase == phrase.downcase
+          return "HUH?! SPEAK UP, SONNY!"
+        elsif phrase == phrase.upcase
+          return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 else
   		return "NO, NOT SINCE 1938!"
